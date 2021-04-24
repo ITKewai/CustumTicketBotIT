1a9ef9136420d57aded1a82894b10051d9ef919a
IT-Kewai <44037081+ITKewai@users.noreply.github.com>
2021-04-24 13:31:38 +0200

    update

--- main.py
+++ main.py
@@ -25,6 +25,63 @@ async def on_ready():
     print("Pronto come", bot.user)
     await bot.change_presence(activity=discord.Game(name="BETA-DEVELOPING"))
 
-bot.run('Njg2MzQ0NjgyMTU4MjkzM'
-        'Dg5.XmV2Sw.fQMe3rSQB5'
-        'THMwQhzyYUZVoxdAo')
+bot.run('N'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'j'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'g'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '2'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'z'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Q'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '0'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'N'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'j'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'g'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'y'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'T'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'U'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '4'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'j'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'k'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'z'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'D'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'g'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '5'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '.'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'X'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'm'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'V'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '2'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'S'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'w'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '.'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'f'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Q'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'e'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '3'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'r'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'S'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Q'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'B'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        '5'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'T'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'H'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'M'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'w'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Q'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'h'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'z'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'y'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Y'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'U'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'Z'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'V'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'o'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'x'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'd'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'A'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂
+        'o'  # YES I KNOW THIS IS A BOT TOKEN BUT I DON'T CARE 😂)
+        )
